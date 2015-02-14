5610afc47b02b26c0da11f761943b3df1529993b
Tom Gurion <nagasaki45@gmail.com>
2015-02-14 11:44:11 +0200

    Chapter 05 solutions are ready

--- ch05/ex01.R
+++ ch05/ex01.R
@@ -1,10 +1,22 @@
 p_ill = 0.001
 p_healthy = 1 - p_ill
-p_positive_given_ill = 0.99
-p_positive_given_healthy = 0.05
+p_positive_g_ill = 0.99
+p_positive_g_healthy = 0.05
 
 # Find p(ill | positive)
 # p(ill | positive) = p(positive | ill) * p(ill) / p(positive)
-p_positive = p_positive_given_ill * p_ill + p_positive_given_healthy * p_healthy
-p_ill_given_positive = p_positive_given_ill * p_ill / p_positive
-print(p_ill_given_positive)  # 0.01943463
+p_positive = p_positive_g_ill * p_ill + p_positive_g_healthy * p_healthy
+p_ill_g_positive = p_positive_g_ill * p_ill / p_positive
+print(p_ill_g_positive)  # 0.01943463
+
+p_ill = p_ill_g_positive  # Updating the prior
+
+p_healthy = 1 - p_ill
+p_negative_g_ill = 1 - p_positive_g_ill
+
+# Find p(ill | negative)
+# p(ill | negative) = p(negative | ill) * p(ill) / p(negative)
+p_positive = p_positive_g_ill * p_ill + p_positive_g_healthy * p_healthy
+p_negative = 1 - p_positive
+p_ill_g_negative = p_negative_g_ill * p_ill / p_negative
+print(p_ill_g_negative)  # 0.0002085862
--- ch05/ex04.R
+++ ch05/ex04.R
@@ -1,23 +1,116 @@
-p_ill = 0.001
-p_healthy = 1 - p_ill
-p_positive_given_ill = 0.99
-p_negative_given_ill = 1 - p_positive_given_ill
-p_positive_given_healthy = 0.05
-p_negative_given_healthy = 1 - p_positive_given_healthy
+source('../utils/HDIofGrid.R')
+source('../utils/BernGrid.R')
 
-# 1st phase, find p(ill | negative)
-# p(ill | negative) = p(negative | ill) * p(ill) / p(negative
-p_negative = p_negative_given_ill * p_ill + p_negative_given_healthy * p_healthy
-p_ill_given_negative = p_negative_given_ill * p_ill / p_negative
-print(paste('1st phase - p(ill | negative): ', p_ill_given_negative))
+theta = seq(0, 1, length=5)     # Sparse teeth for theta.
+p_theta = pmin(theta, 1-theta)  # Triangular shape for p_theta.
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,0),rep(1,1))     # Single flip with 1 head
 
-# Update old values
-p_ill = p_ill_given_negative
-p_healthy = 1 - p_ill
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="None", showHDI=FALSE, showpD=FALSE)
+#------------------------------------------------------------------------------
 
-# 2nd phase, find p(ill | positive)
-# p(ill | positive) = p(positive | ill) * p(ill) / p(positive)
-p_positive = p_positive_given_ill * p_ill + p_positive_given_healthy * p_healthy
-p_ill_given_positive = p_positive_given_ill * p_ill / p_positive
-print(paste('2nd phase - p(ill | positive): ', p_ill_given_positive))
-# Final result: 0.000208586165048544
+theta = seq(0, 1, length=11)    # Sparse teeth for theta.
+p_theta = pmin(theta, 1-theta)  # Triangular shape for p_theta.
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,0),rep(1,1))     # Single flip with 1 head
+
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="None", showHDI=FALSE, showpD=FALSE)
+#------------------------------------------------------------------------------
+
+theta = seq(0, 1, length=1001)  # Fine teeth for theta.
+p_theta = rep(1,length(theta))  # Uniform (horizontal) shape for p_theta.
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,0),rep(1,1))     # Single flip with 1 head
+
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="None", showHDI=FALSE, showpD=FALSE)
+#------------------------------------------------------------------------------
+
+theta = seq(0, 1, length=1001)  # Fine teeth for theta.
+p_theta = rep(0,length(theta))  # Only extremes are possible!
+p_theta[2] = 1                  # Only extremes are possible!
+p_theta[length(p_theta)-1] = 1       
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,0),rep(1,1))     # Single flip with 1 head
+
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="None", showHDI=FALSE, showpD=FALSE)
+#------------------------------------------------------------------------------
+
+
+
+theta = seq(0, 1, length=1001)  # Fine teeth for theta.
+p_theta = pmin(theta, 1-theta)  # Triangular shape for p_theta.
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,3),rep(1,1))     # 25% heads, N=4
+
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="Mode", showHDI=TRUE, showpD=FALSE)
+#------------------------------------------------------------------------------
+
+theta = seq(0, 1, length=1001)  # Fine teeth for theta.
+p_theta = pmin(theta, 1-theta)  # Triangular shape for p_theta.
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+p_theta = p_theta^10            # Sharpen p_theta !
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,3),rep(1,1))     # 25% heads, N=4
+
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="Mode", showHDI=TRUE, showpD=FALSE)
+#------------------------------------------------------------------------------
+
+theta = seq(0, 1, length=1001)  # Fine teeth for theta.
+p_theta = pmin(theta, 1-theta)  # Triangular shape for p_theta.
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+p_theta = p_theta^0.1           # Flatten p_theta !
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,3),rep(1,1))     # 25% heads, N=4
+
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="Mode", showHDI=TRUE, showpD=FALSE)
+#------------------------------------------------------------------------------
+
+
+theta = seq(0, 1, length=1001)  # Fine teeth for theta.
+p_theta = pmin(theta, 1-theta)  # Triangular shape for p_theta.
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,30),rep(1,10))   # 25% heads, N=40
+
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="Mode", showHDI=TRUE, showpD=FALSE)
+#------------------------------------------------------------------------------
+
+theta = seq(0, 1, length=1001)  # Fine teeth for theta.
+p_theta = pmin(theta, 1-theta)  # Triangular shape for p_theta.
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+p_theta = p_theta^10            # Sharpen p_theta !
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,30),rep(1,10))   # 25% heads, N=40
+
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="Mode", showHDI=TRUE, showpD=FALSE)
+#------------------------------------------------------------------------------
+
+theta = seq(0, 1, length=1001)  # Fine teeth for theta.
+p_theta = pmin(theta, 1-theta)  # Triangular shape for p_theta.
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+p_theta = p_theta^0.1           # Flatten p_theta !
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,30),rep(1,10))   # 25% heads, N=40
+
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="Mode", showHDI=TRUE, showpD=FALSE)
+#------------------------------------------------------------------------------
+
+theta = seq(0, 1, length=1000)  # Fine teeth for theta.
+# Two triangular peaks on a small non-zero floor:
+p_theta = c(rep(1,200),seq(1,100,length=50),seq(100,1,length=50),rep(1,200), 
+            rep(1,200),seq(1,100,length=50),seq(100,1,length=50),rep(1,200))
+p_theta = p_theta/sum(p_theta)  # Make p_theta sum to 1.0
+data = c(rep(0,13),rep(1,14)) 
+
+posterior = BernGrid(theta, p_theta, data, plotType="Bars", 
+                     showCentTend="None", showHDI=FALSE, showpD=FALSE)
+#------------------------------------------------------------------------------
